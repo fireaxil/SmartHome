dbda5e1bd21ed9c0847e56c6df019979a955ce99
Christian Koehler <sandvice@gmail.com>
2018-03-17 13:07:34 -0400

    first pull. added gitignore

--- app/index.js
+++ app/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import CssBaseline from 'material-ui/CssBaseline'
 import Typography from 'material-ui/Typography'
+import { createMuiTheme } from 'material-ui/styles'
 
 function App () {
   return (
     <div>
       <CssBaseline />
       <Typography>
-        Ben is a sexy motherfucker
+        Hello world!
       </Typography>
     </div>
   )
